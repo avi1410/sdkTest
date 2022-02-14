9f01c00c2af366056a0ace70136f14d80f80d782
avi1410 <avinashsoni1996.as@gmail.com>
2022-02-14 20:03:08 +0530

    first commit

---
+++ app/src/main/java/com/grayquest/testsdkimplementation/WebAppInterface.java
@@ -0,0 +1,51 @@
+package com.grayquest.testsdkimplementation;
+
+import android.content.Context;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WebAppInterface {
+
+    private static final String TAG = WebAppInterface.class.getSimpleName();
+    Context mContext;
+    String data;
+
+    public WebAppInterface(Context ctx) {
+        this.mContext = ctx;
+    }
+
+    @JavascriptInterface
+    public void showMessageInNative(String data) {
+        Log.e(TAG, "Data: " + data);
+
+        JSONObject testObjet = new JSONObject();
+        try {
+            testObjet.put("test", "eugdugsuidgis");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(mContext, data, Toast.LENGTH_SHORT).show();
+
+//        return testObjet.toString();
+    }
+    @JavascriptInterface
+    public void showMessageInNative2(String data) {
+        Log.e(TAG, "Data: " + data);
+
+        JSONObject testObjet = new JSONObject();
+        try {
+            testObjet.put("test", "asasasa");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(mContext, data, Toast.LENGTH_SHORT).show();
+
+//        return testObjet.toString();
+    }
+}
